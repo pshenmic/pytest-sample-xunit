809e6c3577d386271e5b2d2d21647aee57e2718c
pshenmic <pshenmic@gmail.com>
2019-12-24 11:56:33 +0700

    Format sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -1,24 +1,19 @@
 import pytest
 
-
-def multiply(a,b):
+def multiply(a, b):
     if a == 5:
         raise Exception('MyException')
-    return a*b
+    return a * b
 
+def test_1x1():
+    assert multiply(1, 1) == 1
 
-def test1x1():
-    assert multiply(1,1) == 1
+def test_2x2():
+    assert multiply(2, 2) == 4
 
+def test_3x3():
+    assert multiply(3, 3) == 9
 
-def text2x2():
-    assert multiply(2,2) == 4
-
-
-def test3x3():
-    assert multiply(3,3) == 9
-
-
-def testException():
+def test_exception():
     with pytest.raises(Exception):
         multiply(5, 1)
